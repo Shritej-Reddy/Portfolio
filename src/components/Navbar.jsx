6cd82e646aa187c1f39074280e7ffd9f718c233b
Shritej-Reddy <tejureddy47@gmail.com>
2022-06-27 00:22:57 +0530

    cursor additions

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <h1 className="site-header-name">
+        Shritej Reddy
+      </h1>
+      <span className="site-header-tagline">
+        Web Developer, Designer
+      </span>
+    </div>
+  );
+}
+
+export default Navbar;
